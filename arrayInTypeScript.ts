97dc58f9873e11efcc4e4057a77ae9f2407ac19b
Nasif Khan <nasifmustufakhan7220@gmail.com>
2026-08-17 20:36:02 +0600

    define array type in typeScript

---
+++ arrayInTypeScript.ts
@@ -0,0 +1,32 @@
+let names: string[] = ["Nasif", "Rahim", "Karim"];
+// equivalent to this
+
+let names1: Array<string> = ["Nasif", "Rahim", "Karim"];
+
+
+
+let scores: number[] = [80, 90, 75, 88];
+// equivalent to this
+
+let scores1: Array<number> = [80, 90, 75, 88];
+
+
+let results: boolean[] =[true, false, true]; 
+// equivalent to this
+let results1: Array<boolean> = [true, false, true];
+
+
+
+let students:{
+    name: string;
+    age: number;
+}[] = [
+    {
+        name: "Nasif",
+        age: 24
+    },
+    {
+        name: "Rahim",
+        age: 22
+    }
+]
